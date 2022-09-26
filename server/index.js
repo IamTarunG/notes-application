435a76c14450508db825d854d0506c0acdd5c0a0
IamTarunG <tarungudipalli5@gmail.com>
2022-09-26 15:07:57 +0530

    code for deployment

--- server/index.js
+++ server/index.js
@@ -17,3 +17,10 @@ app.use("/notes", router);
 app.listen(port, () => {
   console.log(`Server listening to port ${port}`);
 });
+app.use('/some-route', require(path.join(__dirname, 'routes', 'noteroutes.js')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+  })
+}
